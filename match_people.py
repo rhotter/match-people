b0a9f1457378d2786fd29f549ec0922906eba891
Raffi Hotter <rhotter@users.noreply.github.com>
2020-06-02 00:04:33 -0400

    working

--- match_people.py
+++ match_people.py
@@ -1,8 +1,73 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from utils import *
+import time
+from itertools import combinations
+from math import factorial
 
-G = nx.DiGraph() # directed graph
+def print_time(f, *args):
+  print("time for: " + f.__name__)
+  start_time = time.time()
+  return_val = f(*args)
+  print(time.time() - start_time)
+  return return_val
+
+spreadsheet_id = '1j7G_PzCfDEn4PDiDlR0hyp180baKbyaThdOxXz9O8HU'
+# spreadsheet_range = 'Learning Experiment!A2:E'
+spreadsheet_range = 'test!A2:E' # 10 people
+
+data = get_data(spreadsheet_id, spreadsheet_range)
+
+"""
+ iterate over combinations of grouping n people into 2 groups
+    returns (group1, group2) where each group is a list of people
+    for each group:
+    build a graph and solve it
+  keep track of the minimum cost and its corresponding result
+"""
+
+# data = print_time(get_data, spreadsheet_id, spreadsheet_range)
+
+weights = [1, 2, 3, 4, 8]
+
+names = {person['name'] for person in data}
+block_size = len(names) // 2
+
+min_cost = None
+min_configuration = None
+
+block_combinations = list(combinations(names, block_size))
+
+costs = []
+for presenters in block_combinations[: len(block_combinations) // 2]: # first half
+  presenters = set(presenters)
+  audience = names.difference(presenters)
+
+  graph1 = build_graph(presenters, audience, data, weights)
+  mincost_flow = nx.max_flow_min_cost(graph1, 'source', 'sink')
+  cost1 = nx.cost_of_flow(graph1, mincost_flow)
+
+  graph2 = build_graph(audience, presenters, data, weights)
+  mincost_flow = nx.max_flow_min_cost(graph2, 'source', 'sink')
+  cost2 = nx.cost_of_flow(graph2, mincost_flow)
+
+  total_cost = cost1 + cost2
+
+  if min_cost is None or total_cost < min_cost:
+    min_cost = total_cost
+    min_configuration = (graph1, graph2)
+  costs.append(total_cost)
+print(min_cost)
+print(min_configuration)
+# plt.hist(costs, bins=20)
+# plt.show()
+# plot_graph(graph1)
+
+
+# print_mincost_flow(mincost_flow)
+# print_out(mincost_flow)
+
+# # print(nx.cost_of_flow(G, mincost_flow))
 
 # data = [
 #   {'name': 'Raffi',
@@ -18,24 +83,3 @@ G = nx.DiGraph() # directed graph
 #   'out': ['Raffi', 'Marley']
 #   }]
 # optimal grouping is (Marley, Santi, Erin), (Raffi, Erin, Santi), (Erin, Raffi, Marley), (Santi, Raffi, Marley)
-
-spreadsheet_id = '1j7G_PzCfDEn4PDiDlR0hyp180baKbyaThdOxXz9O8HU'
-spreadsheet_range = 'Learning Experiment!A2:E'
-
-data = get_data(spreadsheet_id, spreadsheet_range)
-# print(data)
-
-weights = [1, 2, 3, 4, 8] # TODO use [1,2,3,4,8] weights instead
-group_size=3
-
-edges = get_edges(data, weights, group_size)
-G.add_edges_from(edges)
-# print(edges)
-mincost_flow = nx.max_flow_min_cost(G, 'source', 'sink')
-# print(mincost_flow)
-# print_mincost_flow(mincost_flow)
-print_out(mincost_flow)
-
-# # print(nx.cost_of_flow(G, mincost_flow))
-
-# plot_graph(data, G)
--- utils/build_graph_utils.py
+++ utils/build_graph_utils.py
@@ -1,48 +1,43 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-import random
-from .general_utils import in_node, out_node
 
-max_noise = 1e-5
-
-def _get_low_priority_edges(data, weights, random_noise=True):
+def _get_low_priority_edges(presenters, audience, audience_preferences, weights):
   low_priority_weight = weights[-1]
   low_priority_edges = []
 
-  people_names = {person["name"] for person in data}
-  for person in data:
-    in_name = person["name"]
-
+  for audience_member in audience_preferences:
     # add people who aren't yourself and who haven't already been selected
-    for out_name in people_names.difference(set(person["out"])).difference({in_name}):
-      weight = low_priority_weight
-      if random_noise:
-        weight += random.uniform(-max_noise, +max_noise) # perturb weights slightly to get a unique solution
-      low_priority_edges.append((in_node(in_name), out_node(out_name), {'capacity': 1, 'weight': weight}))
+    for presenter_name in presenters.difference(set(audience_member["out"])):
+      low_priority_edges.append((audience_member['name'], presenter_name, {'capacity': 1, 'weight': low_priority_weight}))
   return low_priority_edges
 
-def _get_cross_edges(data, weights, random_noise=True):
+def _get_cross_edges(presenters, audience, audience_preferences, weights):
   cross_edges = []
-  for person in data:
-    for i, out_person_name in enumerate(person['out']):
-      if out_person_name != '':
-        weight = weights[i]
-        if random_noise:
-          weight += random.uniform(-max_noise, +max_noise) # perturb weights slightly to get a unique solution
-        cross_edges.append((in_node(person['name']), out_node(out_person_name), {'capacity': 1, 'weight': weight}))
-  cross_edges.extend(_get_low_priority_edges(data, weights, random_noise))
+  for audience_member in audience_preferences:
+    for i, presenter_name in enumerate(audience_member['out']):
+      if presenter_name in presenters:
+        cross_edges.append((audience_member['name'], presenter_name, {'capacity': 1, 'weight': weights[i]}))
+  cross_edges.extend(_get_low_priority_edges(presenters, audience, audience_preferences, weights))
   return cross_edges
 
-def _get_source_edges(data, group_size):
-  return [('source', in_node(person['name']), {'capacity': group_size - 1, 'weight': 0}) for person in data]
+def _get_source_edges(audience, group_size):
+  return [('source', name, {'capacity': group_size - 1, 'weight': 0}) for name in audience]
 
-def _get_sink_edges(data, group_size):
-  return [(out_node(person['name']), 'sink', {'capacity': group_size - 1, 'weight': 0}) for person in data]
+def _get_sink_edges(presenters, group_size):
+  return [(name, 'sink', {'capacity': group_size - 1, 'weight': 0}) for name in presenters]
 
-def get_edges(data, weights, group_size):
-  source_edges = _get_source_edges(data, group_size)
-  sink_edges = _get_sink_edges(data, group_size)
-  cross_edges = _get_cross_edges(data, weights, random_noise=False)
+def build_graph(presenters, audience, data, weights):
+  group_size=2
+  
+  G = nx.DiGraph() # directed graph
+
+  audience_preferences = [person for person in data if person['name'] in audience]
+  
+  source_edges = _get_source_edges(audience, group_size)
+  sink_edges = _get_sink_edges(presenters, group_size)
+  cross_edges = _get_cross_edges(presenters, audience, audience_preferences, weights)
   edges = source_edges + sink_edges + cross_edges
-  return edges
+
+  G.add_edges_from(edges)
+  return G
