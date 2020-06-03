eaf3bbdd6c55183783a41476e972bc811bebd714
Raffi Hotter <rhotter@users.noreply.github.com>
2020-06-02 23:01:49 -0400

    linear program working

--- match_people.py
+++ match_people.py
@@ -1,9 +1,10 @@
-import networkx as nx
 import matplotlib.pyplot as plt
 from utils import *
 import time
 from itertools import combinations
 from math import factorial
+from pulp import LpVariable, LpProblem, LpMinimize, lpSum, LpStatus, value, LpInteger
+import numpy as np
 
 def print_time(f, *args):
   print("time for: " + f.__name__)
@@ -18,56 +19,89 @@ spreadsheet_range = 'test!A2:E' # 10 people
 
 data = get_data(spreadsheet_id, spreadsheet_range)
 
-"""
- iterate over combinations of grouping n people into 2 groups
-    returns (group1, group2) where each group is a list of people
-    for each group:
-    build a graph and solve it
-  keep track of the minimum cost and its corresponding result
-"""
-
-# data = print_time(get_data, spreadsheet_id, spreadsheet_range)
-
 weights = [1, 2, 3, 4, 8]
 
 names = {person['name'] for person in data}
-block_size = len(names) // 2
+n_people = len(names)
+n_blocks = 2
+block_size = n_people // n_blocks
+"""
+We solve a linear program with 3 constraints:
+(1) Each person listens once:
+    sum_{j,k} [i listens to j at block k] = 1
+    for each person i
 
-min_cost = None
-min_configuration = None
+(2) Each person presents or listens exactly once per block: 
+    sum_{j}[i listens to j at block k] + sum{j}[j listens to i at block k] = 1
+    for each person i and each block k
 
-block_combinations = list(combinations(names, block_size))
+(3) Each block has exactly n/2 presentations:
+    sum_{i,j}[i listens to j at block k] = n/2
 
-costs = []
-for presenters in block_combinations[: len(block_combinations) // 2]: # first half
-  presenters = set(presenters)
-  audience = names.difference(presenters)
-
-  graph1 = build_graph(presenters, audience, data, weights)
-  mincost_flow = nx.max_flow_min_cost(graph1, 'source', 'sink')
-  cost1 = nx.cost_of_flow(graph1, mincost_flow)
-
-  graph2 = build_graph(audience, presenters, data, weights)
-  mincost_flow = nx.max_flow_min_cost(graph2, 'source', 'sink')
-  cost2 = nx.cost_of_flow(graph2, mincost_flow)
-
-  total_cost = cost1 + cost2
-
-  if min_cost is None or total_cost < min_cost:
-    min_cost = total_cost
-    min_configuration = (graph1, graph2)
-  costs.append(total_cost)
-print(min_cost)
-print(min_configuration)
-# plt.hist(costs, bins=20)
-# plt.show()
-# plot_graph(graph1)
+"""
 
 
-# print_mincost_flow(mincost_flow)
-# print_out(mincost_flow)
+x = {(i,j,k): LpVariable(name=f"x[{i},{j},{k}]", cat=LpInteger, lowBound=0, upBound=1) for i in range(n_people) for j in set(range(n_people)).difference(set({i})) for k in range(n_blocks)} # continuous variables by default
+problem = LpProblem("learning-experiment-minimum-cost-schedule", LpMinimize)
 
-# # print(nx.cost_of_flow(G, mincost_flow))
+index_to_person = {i: data[i]['name'] for i in range(n_people)}
+person_to_index = {data[i]['name']: i for i in range(n_people)}
+
+def add_constraints(problem, x, n_people, n_blocks):
+  # each person listens once in total
+  for i in range(n_people):
+    problem += lpSum(x[i,j,k] for j in set(range(n_people)).difference(set({i})) for k in range(n_blocks)) == 1
+  
+  # each person either presents once or listens once in each block
+  for i in range(n_people):
+    for k in range(n_blocks):
+      problem += lpSum(x[i,j,k] + x[j,i,k] for j in set(range(n_people)).difference(set({i}))) == 1
+
+  # each block has exactly n/2 presentations
+  for k in range(n_blocks):
+    problem += lpSum(x[i,j,k] for i in range(n_people) for j in set(range(n_people)).difference(set({i}))) == n_people // 2
+  
+  return problem
+
+def get_edge_costs(data, weights, n_people, n_blocks):
+  edge_costs = {}
+  low_priority_weight = weights[-1]
+  for listener in data:
+    listener_idx = person_to_index[listener['name']]
+    for priority, presenter_name in enumerate(listener['out']):
+      presenter_idx = person_to_index[presenter_name]
+      edge_costs[listener_idx, presenter_idx] = weights[priority]
+    listener_out_indeces = [person_to_index[person_name] for person_name in listener['out']]
+    for presenter_idx in set(range(n_people)).difference(set(listener_out_indeces)).difference(set({listener_idx})):
+      edge_costs[listener_idx, presenter_idx] = low_priority_weight
+  return edge_costs
+
+def add_objective(problem, x, data, weights, n_people, n_blocks):
+  edge_costs = get_edge_costs(data, weights, n_people, n_blocks)
+  # for key, val in edge_costs.items():
+  #   i,j = key
+  #   print(f"{index_to_person[i]} listening to {index_to_person[j]} has value {val}")
+  problem += lpSum(x[i,j,k]*edge_costs[i,j] for i in range(n_people) for j in set(range(n_people)).difference(set({i})) for k in range(n_blocks))
+  return problem
+
+problem = print_time(add_constraints,problem, x, n_people, n_blocks)
+problem = print_time(add_objective,problem, x, data, weights, n_people, n_blocks)
+status = print_time(problem.solve)
+
+print(LpStatus[status])
+print(value(problem.objective))
+
+# for key in x.keys():
+#   i,j,k = key
+#   print(value(x[i,j,k]))
+
+for key in x.keys():
+  i,j,k = key
+  if int(value(x[i,j,k])):
+    presenter_name = index_to_person[j]
+    listener_name = index_to_person[i]
+    print(f"{presenter_name} presents to {listener_name} in round {k}")
+
 
 # data = [
 #   {'name': 'Raffi',
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,1 @@
-from .build_graph_utils import build_graph
-from .plot_graph_utils import plot_graph, print_mincost_flow, print_out
 from .data_loader_utils import get_data
-# import general_utils
