7eb0f0914ed67d791792dc9c9eb7031db917e72f
Raffi Hotter <rhotter@users.noreply.github.com>
2021-02-15 15:05:10 -0800

    Update teaching.py

--- teaching.py
+++ teaching.py
@@ -1,19 +1,13 @@
-from utils import get_data, get_generated_data
+from utils import get_data
 from solver import TeachingSolver
 from settings import spreadsheet_id, spreadsheet_range
 
 data, people_to_topics = get_data(spreadsheet_id, spreadsheet_range, cols=[1,2,3,4,5])
-# print(data)
-# print(data)
-# data = get_generated_data(n_people=11, n_preferences=4)
-# print(data)
-# weights = [1, 2**2, 3**2, 4**2]
-# low_priority_weight = 5**2
 
 weights = [1, 2, 3, 4]
 low_priority_weight = 10
 
-n_blocks = 2
+n_blocks = 2 # currently supports only 2 blocks but passing it into the function anyways...
 
 solver = TeachingSolver(data, n_blocks, weights, low_priority_weight, exclude_presenters=["Jonathan"])
 solver.solve()
